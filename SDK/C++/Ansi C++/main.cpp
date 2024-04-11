5cfcf08635fed15b92592f3dc4582f00b1f27675
Mars Semi Corp <157785052+MarsSemi@users.noreply.github.com>
2024-04-11 14:38:09 +0800

    Update main.cpp

--- SDK/C++/Ansi C++/main.cpp
+++ SDK/C++/Ansi C++/main.cpp
@@ -11,7 +11,7 @@ int main(void)
 	MarsClient _client("test", "test", "justtest");
 
 	if(_client.DoLogin("https://test.mars-cloud.com")) printf("Login SUCCESS\n");
-	if(_client.RegistryDevice("test.com", "test", "temperature", "both.temp")) printf("Reg Device SUCCESS\n");
+	if(_client.RegDevice("test.com", "test", "temperature", "both.temp")) printf("Reg Device SUCCESS\n");
 	if(_client.PutData("test", "metter01", _data)) printf("Put Data SUCCESS\n");
 
 	return 0;
