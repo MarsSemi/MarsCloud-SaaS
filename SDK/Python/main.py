55509e32cd0a6c1f8f971357273c8ed64fbd13f9
Mars Semi Corp <157785052+MarsSemi@users.noreply.github.com>
2024-04-19 18:33:43 +0800

    Update main.py

--- SDK/Python/main.py
+++ SDK/Python/main.py
@@ -1,102 +1,64 @@
-import requests
+
+import MarsClient as marsClient
+import MarsMQTT as marsMQTT
 import json
+import time
 #--------------------------------------------------------------
 #
 #--------------------------------------------------------------
-class MarsClient:
+_Host = "test.mars-cloud.com"
+_Client = marsClient.MarsClient()
+_MQTT = marsMQTT.MarsMQTT()
 #--------------------------------------------------------------
-    def HttpRequest(_self, _req, _payload):
-        try:
-            _headers = { "Content-Type": "application/json", "Cache-Control": "no-cache" }
-
-            if _self.Token != None:
-                _headers["Authentication"] = "Bearer "+_self.Token
-
-            if _payload == None:
-                _resp = requests.get(_req, headers = _headers, timeout = 30)
-            else:
-                _resp = requests.post(_req, data = json.dumps(_payload).encode('utf-8'), headers = _headers, timeout = 30)
-
-            if _resp.status_code == 200:
-                return _resp.text
-        except:
-            return None
+def MQTTCallback(_topic, _msg): 
+    try:
+        print("MQTT Receive : "+_topic)
+        print("MQTT Msg : "+_msg)
+    except:
+        return
 #--------------------------------------------------------------
-    def Login(_self, _host, _usr, _pwd, _proj): 
-        try:
-            _self.Host = _host
-            _self.User = _usr
-            _self.Password = _pwd
-            _self.Proj = _proj
-            _self.Token = None
+def TestAPIs(): 
+    try:
+        if _Client.Login("https://"+_Host, "test", "test", "justtest"):
 
-            _payload = {}
-            _payload['usr'] = _usr
-            _payload['pwd'] = _pwd
-            _payload['proj'] = _proj
+            _resp = _Client.PutData("dev", "test", { "temp": 23.5, "humi": 87 })
+            print("PutData : "+str(_resp))
 
-            _resp = _self.HttpRequest(_host+"/auth/login?", _payload)
+            _resp = json.loads(_Client.GetLastData("dev", "test", 1));
+            _resp = _resp['results'][0]        
+            print("Get Last Data : "+str(_resp))
 
-            if _resp != None:
-                _self.Token = _resp
-                return True
-        except:
-            return False
+            _resp = _Client.RemoveData("dev", "test", _resp['ukey'])
+            print("Remove Data : "+str(_resp))
+
+            _resp = _Client.CallService("service.myService", "/api/hello", None)
+            print("Call Service : "+str(_resp))
+    except:
+        return
+
 #--------------------------------------------------------------
-    def PutData(_self, _uuid, _suid, _data): 
-        try:
-            _payload = {}
-            _payload['uuid'] = _uuid
-            _payload['suid'] = _suid
-            _payload['values'] = [ _data ]
+def TestMQTT(): 
+    try:
+        if _MQTT.Connect(_Host, 8884, _Client.User, _Client.Token, MQTTCallback):
+            print("MQTT Connect OK !")
 
-            _resp = _self.HttpRequest(_self.Host+"/api/put?data", _payload)
-            
-            if _resp != None:
-                return True
-        except:
-            return False
+            _MQTT.Subscribe("+/#")
+            _Client.PushMessage('my/test/msg', { "test": 12345 })
+    except:
+        return
 #--------------------------------------------------------------
-    def GetLastData(_self, _uuid, _suid, _count): 
-        try:
-            _payload = {}
-            _payload['uuid'] = _uuid
-            _payload['suid'] = _suid
-            _payload['count'] = _count
+def main(): 
+    try:
+        TestAPIs()
+        TestMQTT()
 
-            return _self.HttpRequest(_self.Host+"/api/lastdata?method=read", _payload)
-        except:
-            return False
+        while True:
+            time.sleep(0.1)
+    except:
+        return
 #--------------------------------------------------------------
-    def RemoveData(_self, _uuid, _suid, _ukey): 
-        try:
-            _payload = {}
-            _payload['uuid'] = _uuid
-            _payload['suid'] = _suid
-            _payload['ukey'] = _ukey
-
-            _resp = _self.HttpRequest(_self.Host+"/api/del?data", _payload)
-
-            if _resp != None:
-                return True
-        except:
-            return False
+#
 #--------------------------------------------------------------
-    def CallService(_self, _service, _api, _payload): 
-        try:
-            
-            if _service == "service.databroker":
-                return _self.HttpRequest(_self.Host+_api, _payload)
-            else:
-                return _self.HttpRequest(_self.Host+"/services/"+_service+_api, _payload)
-        except:
-            return None
-#--------------------------------------------------------------
-    def PushMessage(_self, _topic, _payload): 
-        try:            
-            _topic = _topic.replace('/', '.')
-            if _self.HttpRequest(_self.Host+"/api/put?message&topic="+_topic, _payload) != None:
-                return True
-        except:
-            return False
+if __name__=="__main__": 
+    main() 
 #--------------------------------------------------------------
